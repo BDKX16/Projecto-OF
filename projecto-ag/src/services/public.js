ba2e26dec5e876919f3f315bc087d0c6fee102d6
bdkx16 <xaveeg@gmail.com>
2024-09-08 12:20:14 -0300

    FIX. fe links

--- projecto-ag/src/services/public.js
+++ projecto-ag/src/services/public.js
@@ -5,7 +5,7 @@ export const login = (username, password) => {
   const controller = loadAbort();
   return {
     call: axios.post(
-      process.env.REACT_APP_BASE_URL + "/api/login",
+      process.env.REACT_APP_BASE_URL + "/login",
       { email: username, password },
       { signal: controller.signal }
     ),
@@ -18,7 +18,7 @@ export const register = (name, username, password) => {
   return {
     call: axios
       .post(
-        process.env.REACT_APP_BASE_URL + "/api/register",
+        process.env.REACT_APP_BASE_URL + "/register",
         { name: name, email: username, password: password },
         { signal: controller.signal }
       )
@@ -32,7 +32,7 @@ export const register = (name, username, password) => {
 export const getContent = () => {
   const controller = loadAbort();
   return {
-    call: axios.get(process.env.REACT_APP_BASE_URL + "/api/content", {
+    call: axios.get(process.env.REACT_APP_BASE_URL + "/content", {
       signal: controller.signal,
     }),
     controller,
@@ -42,7 +42,7 @@ export const getContent = () => {
 export const addContent = (data) => {
   const controller = loadAbort();
   return {
-    call: axios.post(process.env.REACT_APP_BASE_URL + "/api/content", data, {
+    call: axios.post(process.env.REACT_APP_BASE_URL + "/content", data, {
       signal: controller.signal,
     }),
     controller,
@@ -52,7 +52,7 @@ export const addContent = (data) => {
 export const getTemplates = () => {
   const controller = loadAbort();
   return {
-    call: axios.get(process.env.REACT_APP_BASE_URL + "/api/templates", {
+    call: axios.get(process.env.REACT_APP_BASE_URL + "/templates", {
       signal: controller.signal,
     }),
     controller,
@@ -62,7 +62,7 @@ export const getTemplates = () => {
 export const getTemplate = () => {
   const controller = loadAbort();
   return {
-    call: axios.get(process.env.REACT_APP_BASE_URL + "/api/template", {
+    call: axios.get(process.env.REACT_APP_BASE_URL + "/template", {
       signal: controller.signal,
     }),
     controller,
@@ -72,7 +72,7 @@ export const getTemplate = () => {
 export const getVideo = (id) => {
   const controller = loadAbort();
   return {
-    call: axios.get(process.env.REACT_APP_BASE_URL + "/api/content/" + id, {
+    call: axios.get(process.env.REACT_APP_BASE_URL + "/content/" + id, {
       signal: controller.signal,
     }),
     controller,
@@ -84,7 +84,7 @@ export const getPendingPayments = () => {
   return {
     call: axios.get(
       process.env.REACT_APP_BASE_URL +
-        "/api/payments?id=" +
+        "/payments?id=" +
         "66ce67d9a743bd281f1e804b",
       {
         signal: controller.signal,
