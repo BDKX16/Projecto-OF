5de77b35e91767b4a302cd736223a7fcdf08155a
bdkx16 <xaveeg@gmail.com>
2024-09-24 19:46:54 -0300

    FEAT. link video

--- projecto-ag/src/pages/content/contentpage/VideoPage.jsx
+++ projecto-ag/src/pages/content/contentpage/VideoPage.jsx
@@ -5,7 +5,19 @@ const VideoPage = ({ video }) => {
     <div className="main-container">
       <h1>Video Page</h1>
       {/* Aquí puedes agregar el reproductor de video y otros elementos relacionados */}
-      <div className="video-container"></div>
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src={
+            "https://s3.almendragala.com/api/v1/buckets/almen/objects/download?preview=true&prefix=video-test.mp4&version_id=null"
+          }
+          title="Video Player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
       <div className="video-info"></div>
       <div className="video-comments"></div>
     </div>
