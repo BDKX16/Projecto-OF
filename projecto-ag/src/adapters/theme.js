d99aa7409a8feafc248f3878a78c4b48458da93d
bdkx16 <xaveeg@gmail.com>
2024-09-24 22:47:53 -0300

    FEAT. video page styles

--- projecto-ag/src/adapters/theme.js
+++ projecto-ag/src/adapters/theme.js
@@ -1,17 +1,37 @@
 export const createThemeAdapter = (theme) => ({
   palette: {
     primary: {
-      main: theme.find((element) => element.clave === "primary").valor,
+      main:
+        theme.find((element) => element.clave === "primary")?.valor ||
+        "rgba(198, 198, 198, 1)",
       light: "#ff5f52",
     },
     secondary: {
-      main: theme.find((element) => element.clave === "secondary").valor,
+      main:
+        theme.find((element) => element.clave === "secondary")?.valor ||
+        "rgba(198, 198, 198, 1)",
     },
     background: {
-      default: theme.find((element) => element.clave === "background").valor,
-      topbar: theme.find((element) => element.clave === "topbar").valor,
-      sidebar: theme.find((element) => element.clave === "sidebar").valor,
-      footer: theme.find((element) => element.clave === "footer").valor,
+      default:
+        theme.find((element) => element.clave === "background")?.valor ||
+        "rgba(198, 198, 198, 1)",
+      topbar:
+        theme.find((element) => element.clave === "topbar")?.valor ||
+        "rgba(198, 198, 198, 1)",
+      sidebar:
+        theme.find((element) => element.clave === "sidebar")?.valor ||
+        "rgba(198, 198, 198, 1)",
+      footer:
+        theme.find((element) => element.clave === "footer")?.valor ||
+        "rgba(198, 198, 198, 1)",
+    },
+    text: {
+      primary:
+        theme.find((element) => element.clave === "text-primary")?.valor ||
+        "#000000",
+      secondary:
+        theme.find((element) => element.clave === "text-secondary")?.valor ||
+        "#000000",
     },
   },
 });
--- projecto-ag/src/pages/content/contentpage/VideoPage.jsx
+++ projecto-ag/src/pages/content/contentpage/VideoPage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { formatDateToString } from "../../../utils/format-date-to-string";
 
+import { Button, Typography } from "@mui/material";
+
 const VideoPage = ({ video }) => {
   return (
     <div className="main-container">
-      <h1>Video Page</h1>
       {/* Aquí puedes agregar el reproductor de video y otros elementos relacionados */}
       <div className="video-container">
         <iframe
-          width="560"
-          height="315"
+          width="100%"
+          height="100%"
           src={
             video.videoUrl ||
             "https://s3.almendragala.com/api/v1/buckets/almen/objects/download?preview=true&prefix=video-test.mp4&version_id=null"
@@ -20,11 +21,23 @@ const VideoPage = ({ video }) => {
         ></iframe>
       </div>
       <div className="video-info">
-        <h2>{video.title + " - " + formatDateToString(video.date)}</h2>
-        <p>{video.description}</p>
+        <Typography
+          component="h1"
+          variant="h1"
+          fontWeight={400}
+          color="text.primary"
+        >
+          {video.title}
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary">
+          {formatDateToString(video.date)}
+        </Typography>
+        <Typography variant="body1" fontWeight={500} color="text.secondary">
+          {video.description}
+        </Typography>
       </div>
-      <div className="video-comments"></div>
     </div>
   );
 };
+
 export default VideoPage;
