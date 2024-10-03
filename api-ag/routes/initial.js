b8ca02a3a48128c325e8c8114968acc3690bf179
bdkx16 <xaveeg@gmail.com>
2024-10-03 00:03:16 -0300

    deploy

--- api-ag/routes/initial.js
+++ api-ag/routes/initial.js
@@ -13,7 +13,7 @@ router.get("/webcontent", async (req, res) => {
   try {
     let template = await Template.findOne({
       nullDate: null,
-      active: false,
+      active: true,
       $or: [{ validityFrom: null }, { validityFrom: { $lt: new Date() } }],
       $or: [{ validityTo: null }, { validityTo: { $gt: new Date() } }],
     });
